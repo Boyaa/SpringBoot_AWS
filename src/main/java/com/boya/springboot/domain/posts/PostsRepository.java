3a23a5fa64665667cf04856c21c79edf7e4a6ac6
Boyaa <cbh0548@icloud.com>
2024-02-18 19:16:39 +0900

    Add Google Login_1

--- src/main/java/com/boya/springboot/domain/posts/PostsRepository.java
+++ src/main/java/com/boya/springboot/domain/posts/PostsRepository.java
@@ -1,5 +1,6 @@
 package com.boya.springboot.domain.posts;
 
+import com.boya.springboot.domain.posts.Posts;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
