ae7c53f1e3f5fd0d9d1c829c6baaceaa61585681
Boyaa <cbh0548@icloud.com>
2024-02-18 12:38:06 +0900

    Add post-update and post-delete func

--- src/main/java/com/boya/springboot/domain/posts/PostsRepository.java
+++ src/main/java/com/boya/springboot/domain/posts/PostsRepository.java
@@ -1,9 +1,17 @@
 package com.boya.springboot.domain.posts;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+
 public interface PostsRepository extends JpaRepository<Posts, Long> {
 
     /* 인터페이스 생성 후 JpaRepository<Entity 클래스, PK 타입> 상속하면 기본 CRUD 메소드 자동 생성
        Entity 클래스와 Entity Repository 는 함께 위치해야 한다.
      */
+
+    @Query("SELECT p FROM Posts p ORDER BY p.id DESC")
+    List<Posts> findAllDesc();
+
 }
--- src/main/java/com/boya/springboot/service/posts/PostsService.java
+++ src/main/java/com/boya/springboot/service/posts/PostsService.java
@@ -2,12 +2,16 @@ package com.boya.springboot.service.posts;
 
 import com.boya.springboot.domain.posts.Posts;
 import com.boya.springboot.domain.posts.PostsRepository;
+import com.boya.springboot.web.dto.PostsListResponseDto;
 import com.boya.springboot.web.dto.PostsResponseDto;
 import com.boya.springboot.web.dto.PostsSaveRequestDto;
 import com.boya.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor // final이 선언된 모든 필드를 인자값으로 하는 생성자를 생성해줌
 @Service
@@ -33,4 +37,18 @@ public class PostsService {
 
         return new PostsResponseDto(entity);
     }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream().map(PostsListResponseDto::new)
+                              .collect(Collectors.toList());
+        // postsRepository로 넘어온 Posts의 Stream을 map을 통해 PostsListResponseDto 변환 후 list로 반환하는 메소드
+    }
+
+    @Transactional
+    public void delete (Long id) {
+        Posts posts = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다.  id=" + id));
+        postsRepository.delete(posts);
+    }
+
 }
