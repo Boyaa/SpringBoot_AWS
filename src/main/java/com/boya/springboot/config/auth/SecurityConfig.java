a92a6a73bc038d7de7736948c33be0e764fcd9a7
Boyaa <cbh0548@icloud.com>
2024-02-20 22:24:49 +0900

    Add Google Login_2

--- src/main/java/com/boya/springboot/config/auth/SecurityConfig.java
+++ src/main/java/com/boya/springboot/config/auth/SecurityConfig.java
@@ -11,23 +11,23 @@ import org.springframework.security.oauth2.client.userinfo.CustomUserTypesOAuth2
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-    private final CustomUserTypesOAuth2UserService customUserTypesOAuth2UserService;
+    private final CustomOAuth2UserService customOAuth2UserService;
 
     @Override
     protected  void configure(HttpSecurity http) throws Exception {
         http.csrf().disable()
                    .headers().frameOptions().disable()
                    .and()
-                        .authorizeRequests()
+                        .authorizeRequests()//url별 권한관리 옵션 시작, .antMatchers (권한관리 대상 지정 옵션) 사용하기 위해 선언
                         .antMatchers("/","/css/**","/images/**","/js/**","/h2-console/**").permitAll()
                         .antMatchers("/api/v1/**").hasRole(Role.USER.name())
-                        .anyRequest().authenticated()
+                        .anyRequest().authenticated()  // anyRequest() = else
                    .and()
                         .logout()
                             .logoutSuccessUrl("/")
                    .and()
                         .oauth2Login()
-                            .userInfoEndpoint()
-                                .userService(customUserTypesOAuth2UserService);
+                            .userInfoEndpoint() //로그인 성공 이후 사용자 정보 가져올 때의 설정 담당
+                                .userService(customOAuth2UserService);
     }
 }
--- src/main/java/com/boya/springboot/domain/user/UserRepository.java
+++ src/main/java/com/boya/springboot/domain/user/UserRepository.java
@@ -5,5 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-    Optional<User> findByEmail(String email);
+    Optional<User> findByEmail(String email); //  Optional은 값이 없는 경우를 표현하기 위한 클래스
+                                              //email 통해 신규 가입 여부 판단
 }
