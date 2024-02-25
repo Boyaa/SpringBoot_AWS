0a2b8df1ebe17fff4e8aa15e4f217e65958d48b2
Boyaa <cbh0548@icloud.com>
2024-02-25 19:21:15 +0900

    Refactoring with Annotation. Add Naver Login

--- src/main/java/com/boya/springboot/web/indexController.java
+++ src/main/java/com/boya/springboot/web/indexController.java
@@ -1,5 +1,6 @@
 package com.boya.springboot.web;
 
+import com.boya.springboot.config.auth.LoginUser;
 import com.boya.springboot.config.auth.dto.SessionUser;
 import com.boya.springboot.service.posts.PostsService;
 import com.boya.springboot.web.dto.PostsResponseDto;
@@ -19,9 +20,8 @@ public class indexController {
         private final HttpSession httpSession;
 
         @GetMapping("/")
-        public String index(Model model) {
+        public String index(Model model, @LoginUser SessionUser user) {
             model.addAttribute("posts",postsService.findAllDesc());
-            SessionUser user = (SessionUser) httpSession.getAttribute("user");
             if (user != null) {
                 model.addAttribute("usersName", user.getName());
             }
